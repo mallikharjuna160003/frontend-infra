b850f334eb886a5d05a67b0c186ad6f618d89225
mallikharjuna160003 <74324685+mallikharjuna160003@users.noreply.github.com>
2024-11-04 14:36:48 +0530

    Update provider.tf
    updated the provider.tf file

--- provider.tf
+++ provider.tf
@@ -1,4 +1,12 @@
 provider "aws" {
-  alias  = "us-east-1"
-  region = "us-east-1"
+  alias  = "us-west-2"
+  region = "us-west-2"
+}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"  // Specify the version as needed
+    }
+  }
 }
