bfce57bfb8155c7d3f337b8712338e474b3d1a20
mallikharjuna160003 <74324685+mallikharjuna160003@users.noreply.github.com>
2024-11-04 14:56:39 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,4 @@
 terraform {
-
-  backend "s3" {
-    bucket         = "frontend-s3-bucket-123456" // Your S3 bucket for storing the state
-    key            = "terraform/state" // The path to your state file in the bucket
-    region         = "us-west-2" // The region where your S3 bucket is located
-    encrypt        = true // Encrypt the state file at rest
-    dynamodb_table = "frontend-lockfile" // Your DynamoDB table for state locking
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
