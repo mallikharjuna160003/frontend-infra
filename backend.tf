66ee5e44e2f1b21f3cbb269f250f9895e28e4b9d
mallikharjuna160003 <74324685+mallikharjuna160003@users.noreply.github.com>
2024-11-04 15:00:28 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "frontend-s3-bucket-123456"
     key            = "terraform/state"
-    region         = "us-east-1"
+    region         = "us-west-2"
     encrypt        = true
     dynamodb_table = "frontend-lockfile"
   }
